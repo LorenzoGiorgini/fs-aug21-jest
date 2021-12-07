aa482a987dab2346a5e4b9c774e4a68a0a55bd6a
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-12-07 14:59:41 +0100

    tests now works

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose"
 
 const port = process.env.PORT || 3000;
 
-console.log(process.env.MONGO_URL)
-
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log(`Connected to Mongo`);
--- src/products/index.js
+++ src/products/index.js
@@ -1,17 +1,47 @@
-import express from "express"
-import { ProductModel } from "./model.js"
+import express from "express";
+import { ProductModel } from "./model.js";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
 productsRouter
-    .get('/', async (req, res) => {
-        const products = await ProductModel.find({})
-        res.send(products)
-    })
-    .post("/", async (req, res) => {
-        const product = new ProductModel(req.body)
-        await product.save()
-        res.status(201).send(product)
-    })
+  .get("/", async (req, res) => {
+    const products = await ProductModel.find({});
+    res.status(200).send(products);
+  })
+  .post("/", async (req, res) => {
+    const product = new ProductModel(req.body);
+    await product.save();
+    res.status(201).send(product);
+  });
 
-export default productsRouter
+
+productsRouter
+.get("/:id", async (req, res) => {
+    const product = await ProductModel.findById(req.params.id);
+    if(product) {
+      res.status(200).send(product);
+
+    } else {
+      res.status(404).send({message: "Product not found"});
+    }
+})
+.delete("/:id", async (req, res) => {
+    const product = await ProductModel.findByIdAndDelete(req.params.id);
+    if(product) {
+      res.status(204).send({});
+
+    } else {
+      res.status(404).send({message: "Product not found"});
+    }
+})
+.put("/:id", async (req, res) => {
+    const product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if(product) {
+      res.status(203).send(product);
+
+    } else {
+      res.status(404).send({message: "Product not found"});
+    }
+})
+
+export default productsRouter;
